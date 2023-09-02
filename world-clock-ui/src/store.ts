bb8953a39a70cec08f4149b774185295e2d5aec9
Andres Sampo <andres.sampo@bitlogic.io>
2023-09-01 23:14:02 -0300

    Autocomplete refactor

--- world-clock-ui/src/store.ts
+++ world-clock-ui/src/store.ts
@@ -2,18 +2,10 @@ import { create } from 'zustand';
 
 interface State {
   selectedOptions: string[];
-  filteredOptions: string[];
-  // addOption: (option: string) => void;
-  // removeOption: (option: string) => void;
   setSelectedOptions: (options: string[]) => void;
-  setFilteredOptions: (options: string[]) => void; // Nueva función para establecer filteredOptions
 }
-
+//TODO Maybe we can dont use zustand?
 export const useStore = create<State>((set) => ({
   selectedOptions: [],
-  filteredOptions: [],
-  // addOption: (option: string) => set((state) => ({ selectedOptions: [...state.selectedOptions, option] })),
-  // removeOption: (option: string) => set((state) => ({ selectedOptions: state.selectedOptions.filter((item) => item !== option) })),
-  setSelectedOptions: (options: string[]) => set({ selectedOptions: options }),
-  setFilteredOptions: (options: string[]) => set({ filteredOptions: options })
+  setSelectedOptions: (options: string[]) => set({ selectedOptions: options })
 }));
